6c68fa354cffac7a74b8a51eec9a805bc2710f44
HOBIN2 <96759097+HOBIN2@users.noreply.github.com>
2021-12-28 14:08:58 +0900

    Update 00.py

--- 이호빈/00.py
+++ 이호빈/00.py
@@ -1,1 +1,15 @@
-
+#백준 단계별 문제 8958번
+N = int(input())
+score = 0
+total_score = 0
+for i in range(N):
+    result = list(input())
+    for quiz in result:
+        if quiz == "O":
+            score += 1
+            total_score += score
+        else:
+            score = 0
+    print(total_score)
+    total_score = 0
+    score = 0
