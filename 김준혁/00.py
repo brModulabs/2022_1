67ec19de79ce14ba8ce32f1fe03e50857a598337
JHyeok01 <89198567+JHyeok01@users.noreply.github.com>
2021-12-28 14:06:07 +0900

    Update 00.py

--- 김준혁/00.py
+++ 김준혁/00.py
@@ -1,1 +1,13 @@
+#백준 9498번: 시험 성적
+score = int(input())
 
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else:
+    print("E")
