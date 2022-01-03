02106619b672511dbf032ff3c8a662909ae8bdc9
brModulabs <byeongryul.lee@modulabs.co.kr>
2022-01-03 12:43:28 +0900

    ✅ 00.py 변경

--- 병률/00.py
+++ 병률/00.py
@@ -1,4 +1,13 @@
-print()
-print(1)
-print(1 + 1)
-print('Hello')
+A,B,C = input().split()
+
+바보 = '''(A+B)%C
+((A%C) + (B%C))%C
+(A*B)%C
+((A%C) * (B%C))%C'''
+
+바보=바보.replace('A',A)
+바보=바보.replace('B',B)
+바보=바보.replace('C',C)
+
+for re in 바보.split('\n'):
+    print(eval(re))
